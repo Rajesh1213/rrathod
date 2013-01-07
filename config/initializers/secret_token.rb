c46f528d4dad3a5d902ef617b59bfde67f2af3c2
Rajesh Rathod <rajesh.rathod@corporategovernancerisk.com>
2013-01-08 00:37:47 +0530

    personal site first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rajeshrathod::Application.config.secret_token = '6254fbe32da93f56cbc1b4c5ed5c981f139cfe09b4684ebff7bda5e0e0021dcb10947c4cb873e2d6bdf6f4f2946703fbbb52492342b574faab1194a2a5223d11'
